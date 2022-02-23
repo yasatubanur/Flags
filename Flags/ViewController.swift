645f0cb5f39aee685bfb1ef5e2afe3ca4ad043da
Tuba Nur <yasatubanur@gmail.com>
2022-02-23 13:44:07 +0300

    Initial Commit

---
+++ Flags/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flags
+//
+//  Created by Tuba Nur YAŞA on 23.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
