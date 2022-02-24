34253966e72f9fc96eda0c9c7ac05d54bd61e6fe
Tuba Nur <yasatubanur@gmail.com>
2022-02-24 22:13:08 +0300

    detailViewController created.

---
+++ Flags/DetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailView.swift
+//  Flags
+//
+//  Created by Tuba Nur YAŞA on 24.02.2022.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
