87a9b0dace8ccb90a2c390f55fe26ece337afabf
Tuba Nur <yasatubanur@gmail.com>
2022-02-25 21:16:45 +0300

    homeVC created.

---
+++ Flags/HomeVC.swift
@@ -0,0 +1,39 @@
+//
+//  HomeVC.swift
+//  Flags
+//
+//  Created by Tuba Nur YAŞA on 25.02.2022.
+//
+
+import UIKit
+
+class HomeVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var tableView: UITableView!
+    let countries =  ["estonia","france","germany","irland","italy","monaco","nigeria","poland","russia","spain","uk","us"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        self.title = "Flags"
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "defaultCell", for:indexPath)
+        cell.textLabel?.text = countries[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return countries.count
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        let destination = storyboard.instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
+        navigationController?.pushViewController(destination, animated: true)
+    }
+}
--- Flags/ViewController.swift
+++ Flags/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var tableView: UITableView!
+    
+    var countries = [String]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var title = "Flags"
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Share your friend", style: .plain, target: self, action: #selector(suggest))
+        
+        countries += ["estonia","france","germany","irland","italy","monaco","nigeria","poland","russia","spain","uk","us"]
         // Do any additional setup after loading the view.
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+
+        for item in items {
+            if item.hasPrefix("nssl") {
+                // this is a picture to load!
+            }
+        }
     }
-
-
+    @objc func suggest(){
+        let sharelink = "https://github.com/yasatubanur"
+        let vc = UIActivityViewController(activityItems: [sharelink], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc,animated: true)
+    }
+    
 }
 
